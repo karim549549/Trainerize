b2204285d14eb5c638f33e7c18c2a112e09ff43a
karim khaled <karimkhaled549@gmail.com>
2024-12-03 17:10:08 +0200

    hamdlah

--- src/main.ts
+++ src/main.ts
@@ -20,8 +20,8 @@ async function bootstrap() {
   .setVersion('1.0')
   .build();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('swagger', app, document);
+  SwaggerModule.setup(process.env.SWAGGER_PATH ||'swagger' , app, document);
   
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
